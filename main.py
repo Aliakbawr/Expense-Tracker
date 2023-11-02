e2fbfcaeb0e5ca60adb1f991746e92613ea18510
Aliakbar Ahrari <ahr04624@gmail.com>
2023-11-02 11:48:08 +0330

    Sign-Up and Sign-in using sqlite3 added

--- main.py
+++ main.py
@@ -1,20 +1,71 @@
-from Panels.main_panel import user_plan_chooser
-from User.User import User
+import sqlite3
+
+# Connect to the database
+conn = sqlite3.connect('users.db')
+
+# Create a table for storing user information
+conn.execute('''CREATE TABLE IF NOT EXISTS users
+             (username TEXT PRIMARY KEY NOT NULL,
+             password TEXT NOT NULL,
+             last_login TEXT);''')
+
+
+# Sign up function
+def signup():
+    username = input('Enter your username: ')
+    password = input('Enter your password: ')
+    conn.execute(f"INSERT OR IGNORE INTO users (username, password,last_login) VALUES ('{username}', '{password}', NULL)")
+    conn.commit()
+    print('Account created successfully!')
+    user_panel(username)
+    starter()
+
+
+# Sign in function
+def signin():
+    username = input('Enter your username: ')
+    password = input('Enter your password: ')
+    cursor = conn.execute(f"SELECT * FROM users WHERE username='{username}' AND password='{password}'")
+    if len(cursor.fetchall()) > 0:
+        print('Login successful!')
+        # Update the database with the login time
+        conn.execute(f"UPDATE users SET last_login=datetime('now') WHERE username='{username}'")
+        conn.commit()
+        user_panel(username)
+
+    else:
+        print('Invalid username or password')
+        starter()
 
 
 def starter():
-    user_input = input('Enter the username:')
-    password_input = input('Enter the password:')
-    if user_input == 'a':
-        if password_input == '1':
-            print('successful login')
-            user_plan_chooser(user)
+    while True:
+        print('1. Sign up')
+        print('2. Sign in')
+        choice = input('Enter your choice: ')
+        if choice == '1':
+            signup()
+        elif choice == '2':
+            signin()
         else:
-            print('incorrect password')
-    else:
-        print('user not found')
+            print('Invalid choice')
 
 
-user = User('a', '1')
-print(user.name)
+def user_panel(username):
+    user_input = int(input('Select:\n1)Beginner\t2)Serious Budgeter\t3)Business-Owner\t4)Sign out'))
+    match user_input:
+        case 1:
+            print('Choose a plan:\n1)Envelope')
+        case 2:
+            print('Choose a plan:\n1)Envelope')
+        case 3:
+            print('Choose a plan:\n1)Envelope')
+        case 4:
+            print('Returning ...')
+            starter()
+        case _:
+            print('Wrong input')
+            user_panel(username)
+
+
 starter()
